8c4a8f7bd9007f7fdeb306971c59eef3344f1260
Marc Brown <marcsmobile.brown@googlemail.com>
2025-03-07 22:09:25 +0000

    Update secrets_manager.tf

--- secrets_manager.tf
+++ secrets_manager.tf
@@ -2,7 +2,7 @@ resource "aws_secretsmanager_secret" "db_credentials" {
   name        = "rds-db-credentials"
   description = "Database credentials for Terraform-managed RDS"
 
-  tags = {
+  tags = {https://github.com/Brownster/terraform_aws_connect/edit/main/secrets_manager.tf
     Environment = "Terraform"
   }
 }
@@ -14,3 +14,25 @@ resource "aws_secretsmanager_secret_version" "db_credentials_version" {
     password = "SuperSecureP@ssword!"
   })
 }
+
+data "aws_secretsmanager_secret_version" "db_credentials" {
+  secret_id = aws_secretsmanager_secret.db_credentials.id
+}
+
+locals {
+  db_creds = jsondecode(data.aws_secretsmanager_secret_version.db_credentials.secret_string)
+}
+
+resource "aws_db_instance" "default" {
+  count             = var.enable_rds ? 1 : 0
+  engine           = "mysql"
+  instance_class   = "db.t3.micro"
+  allocated_storage = 20
+  db_name          = "mydb"
+  username        = local.db_creds["username"]
+  password        = local.db_creds["password"]
+  skip_final_snapshot = true
+  publicly_accessible = false
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+}
+
