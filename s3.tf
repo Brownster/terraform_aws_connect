c2a9edc3daf4c65797e56f7231522f64350501ca
Marc Brown <marcsmobile.brown@googlemail.com>
2025-03-07 22:12:26 +0000

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -9,3 +9,29 @@ resource "aws_s3_bucket_public_access_block" "bucket_access" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_policy" "terraform_state_policy" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowTerraformOnly"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource  = [
+          "arn:aws:s3:::${var.backend_bucket_name}",
+          "arn:aws:s3:::${var.backend_bucket_name}/*"
+        ]
+        Condition = {
+          StringNotEquals = {
+            "aws:PrincipalArn" = aws_iam_role.terraform_role.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
